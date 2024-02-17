3bf71fd01fd3b1f0b996550b2857a78470d2443d
Ghulam Mustafa <mustafahanif275@gmail.com>
2024-02-17 20:55:25 +0500

    new work work

---
+++ work js/web.js
@@ -0,0 +1,16 @@
+// alert('Well Come')
+
+
+
+// var a ="salam"
+// alert(a)
+
+// var a = "Hi I am Ghulam mustafa"
+// var a = "mustafa"
+
+// alert(a)
+
+var b = "abc"
+b = 123
+alert(b)
+prompt(b)
